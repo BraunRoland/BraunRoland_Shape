86c93094669261a7ab44a5511b2843962c7f7ab4
Braun Roland <rolibraun94@gmail.com>
2025-03-21 12:40:37 +0100

    Shape kesz

---
+++ Shape/Shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal abstract class Shape
+    {
+        bool isHoley;
+        string color;
+
+        protected Shape(bool isHoley, string color)
+        {
+            this.isHoley = isHoley;
+            this.color = color;
+        }
+
+        protected Shape(string color)
+        {
+            this.isHoley = false;
+            this.color = color;
+        }
+
+        public string Color { get => color; set => color = value; }
+
+        protected void MakeHoley()
+        {
+            isHoley = true;
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+
+        public override string ToString()
+        {
+            return $"{color} - {isHoley} - K: {Perimeter()} - T: {Area()}";
+        }
+    }
+}
